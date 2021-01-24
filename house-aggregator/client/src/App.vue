e38b1a0f9a69fab72aec20065d8b46689528fd6d
mollyshillabeer <mollyshillabeer@gmail.com>
2021-01-24 01:38:19 -0500

    anything

--- house-aggregator/client/src/App.vue
+++ house-aggregator/client/src/App.vue
@@ -1,32 +1,62 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <b-navbar>
+        <b-navbar-brand href="#">
+          <img src="./assets/house.png" alt="icon" width="60px" class="d-inline-block">
+          <!--<router-link to="/"> -->
+          Student Housing
+          <!--</router-link> -->
+        </b-navbar-brand>
+      </b-navbar>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+
+* {
+  margin: 0px;
+  border: 0px;
+}
+
+*, *::before, *::after {
+
+  box-sizing: border-box;
+}
+
+img {
+    margin-right: 15px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #182430;
+  background-color: #f8f9fa;
 }
 
 #nav {
-  padding: 30px;
+  padding: 15px;
+  background-color: #054050;
+  color: white;
+  margin-right: auto;
+  margin-left: auto;
+  width: auto;
+  flex-wrap: nowrap;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #adc8e6;
+  text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+  text-decoration: none;
 }
 </style>
